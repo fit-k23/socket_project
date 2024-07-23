56fe0795a5988879b4abae89c26f16393ef5927d
NTT1906 <nttispay@gmail.com>
2024-07-24 03:20:11 +0700

    first ver

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+import json
+
+class PRIORITY:
+    NORMAL = 0
+    HIGH = 1
+    CRITICAL = 2
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
