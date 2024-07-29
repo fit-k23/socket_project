c749b41e6901d4ac29ce4d1bfdd5c67f9bc8d0ee
NTT1906 <nttispay@gmail.com>
2024-07-29 22:05:35 +0700

    ADD: changes

---
+++ priority.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class PRIORITY(Enum):
+    NORMAL = 0
+    HIGH = 1
+    CRITICAL = 2
--- reset.py
+++ reset.py
@@ -3,3 +3,4 @@ import sys
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5000
 
+# netstat -ano | findstr 5000
